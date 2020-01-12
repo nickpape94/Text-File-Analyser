a3270c09e024d4cfb9d497da867844e7b7b3352e
nickpape94 <nick-_p@hotmail.com>
2020-01-12 20:23:18 +0000

    project with 71% coverage

--- src/main/java/com/nick/synalogik/service/FileReading.java
+++ src/main/java/com/nick/synalogik/service/FileReading.java
@@ -1,4 +1,5 @@
 package com.nick.synalogik.service;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,20 +13,27 @@ import java.util.stream.Collectors;
 
 public class FileReading {
 
-	 public static String fileInput = "C:\\Users\\Nick-\\Java-Projects\\test.txt";
-	 String splitDeliminator = "[,*;:.@?!_'\\s]+";
+	public static String fileInput = "C:\\Users\\Nick-\\Java-Projects\\test3.txt";
+	String splitDeliminator = "[,*;:.@?!_'\\s]+";
 
 	public Scanner readFile() throws FileNotFoundException {
 		Scanner in = new Scanner(new File(fileInput));
 		return in;
-		
+
 	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		new FileReading().wordCount();
-		new FileReading().averageWordLength();
-		new FileReading().numWordsOfLength();
+		System.out.println("Word count: " + new FileReading().wordCount());
+		System.out.println("Average word length: " + new FileReading().averageWordLength());
+		System.out.println("Number of words of length 1: " + new FileReading().numWordsOfLength()[0]);
+		System.out.println("Number of words of length 2: " + new FileReading().numWordsOfLength()[1]);
+		System.out.println("Number of words of length 3: " + new FileReading().numWordsOfLength()[2]);
+		System.out.println("Number of words of length 4: " + new FileReading().numWordsOfLength()[3]);
+		System.out.println("Number of words of length 5: " + new FileReading().numWordsOfLength()[4]);
+		System.out.println("Number of words of length 7: " + new FileReading().numWordsOfLength()[5]);
+		System.out.println("Number of words of length 10: " + new FileReading().numWordsOfLength()[6]);
+		System.out.println("The most frequently occurring word length is " + new FileReading().numWordsOfLength()[7] + " for word lengths " + new FileReading().maxRepeatedSizes());
 
 	}
 
@@ -36,11 +44,10 @@ public class FileReading {
 			listwords.addAll(Arrays.asList(in.next().split(splitDeliminator)));
 		}
 
-		System.out.println("Word count: " + listwords.size());
 		return listwords.size();
-		
+
 	}
-	
+
 	public double averageWordLength() throws FileNotFoundException {
 		Scanner in = readFile();
 		List<String> listwords = new ArrayList<>();
@@ -49,7 +56,6 @@ public class FileReading {
 			listwords.addAll(Arrays.asList(in.next().split(splitDeliminator)));
 		}
 
-		// word_count = word_count + listwords.size();
 		for (String word : listwords) {
 			letter_count += word.length();
 		}
@@ -57,14 +63,14 @@ public class FileReading {
 		double average = letter_count / (double) listwords.size();
 		average = Math.floor(average * 1000) / 1000;
 
-		System.out.println("Average word length: " + average);
 		return average;
-		
+
 	}
 
-	public void numWordsOfLength() throws FileNotFoundException {
+	public int[] numWordsOfLength() throws FileNotFoundException {
 		Scanner in = readFile();
 		List<String> listwords = new ArrayList<>();
+		int[] arr = new int[8];
 
 		while (in.hasNext()) {
 			listwords.addAll(Arrays.asList(in.next().split(splitDeliminator)));
@@ -84,38 +90,30 @@ public class FileReading {
 				if (word.length() == 1) {
 					numWordsOfLength_1++;
 					wordsSorted.add(word);
-
 				}
 				if (word.length() == 2) {
 					numWordsOfLength_2++;
 					wordsSorted.add(word);
-
 				}
 				if (word.length() == 3) {
 					numWordsOfLength_3++;
 					wordsSorted.add(word);
-
 				}
 				if (word.length() == 4) {
 					numWordsOfLength_4++;
 					wordsSorted.add(word);
-
 				}
-
 				if (word.length() == 5) {
 					numWordsOfLength_5++;
 					wordsSorted.add(word);
-
 				}
 				if (word.length() == 7) {
 					numWordsOfLength_7++;
 					wordsSorted.add(word);
-
 				}
 				if (word.length() == 10) {
 					numWordsOfLength_10++;
 					wordsSorted.add(word);
-
 				}
 			}
 
@@ -124,30 +122,46 @@ public class FileReading {
 
 			int mostFrequentWordLength = Collections.max(numWords0fLength);
 
-			String[] test = wordsSorted.toArray(new String[0]);
+			arr[0] = numWordsOfLength_1;
+			arr[1] = numWordsOfLength_2;
+			arr[2] = numWordsOfLength_3;
+			arr[3] = numWordsOfLength_4;
+			arr[4] = numWordsOfLength_5;
+			arr[5] = numWordsOfLength_7;
+			arr[6] = numWordsOfLength_10;
+			arr[7] = mostFrequentWordLength;
 
-			List<Integer> maxRepeatingSizes = Arrays.stream(test)
-					.collect(Collectors.groupingBy(String::length, Collectors.counting())).entrySet().stream()
-					.collect(Collectors.groupingBy(Map.Entry::getValue,
-							Collectors.mapping(Map.Entry::getKey, Collectors.toList())))
-					.entrySet().stream().max(Map.Entry.comparingByKey()).get().getValue();
-			
-			
-
-			//System.out.println(wordsSorted);
-			//System.out.println(in);
-			System.out.println("Number of words of length 1: " + numWordsOfLength_1);
-			System.out.println("Number of words of length 2: " + numWordsOfLength_2);
-			System.out.println("Number of words of length 3: " + numWordsOfLength_3);
-			System.out.println("Number of words of length 4: " + numWordsOfLength_4);
-			System.out.println("Number of words of length 5: " + numWordsOfLength_5);
-			System.out.println("Number of words of length 7: " + numWordsOfLength_7);
-			System.out.println("Number of words of length 10: " + numWordsOfLength_10);
-			System.out.println("The most frequently occurring word length is " + mostFrequentWordLength
-					+ " for word lengths " + maxRepeatingSizes);
-			
-		
 		}
+
+		return arr;
+	}
+
+	public List<Integer> maxRepeatedSizes() throws FileNotFoundException {
+		Scanner in = readFile();
+		List<String> listwords = new ArrayList<>();
+
+		while (in.hasNext()) {
+			listwords.addAll(Arrays.asList(in.next().split(splitDeliminator)));
+		}
+		
+
+		List<String> wordsSorted = new ArrayList<String>();
+
+		for (String word : listwords) {
+			if (word.length() == 1 || word.length() == 2 || word.length() == 3 || word.length() == 4 || word.length() == 5 || word.length() == 7 || word.length() == 10) {
+				wordsSorted.add(word);
+			}
+		}
+
+		String[] test = wordsSorted.toArray(new String[0]);
+
+		List<Integer> maxRepeatingSizes = Arrays.stream(test)
+				.collect(Collectors.groupingBy(String::length, Collectors.counting())).entrySet().stream()
+				.collect(Collectors.groupingBy(Map.Entry::getValue,
+						Collectors.mapping(Map.Entry::getKey, Collectors.toList())))
+				.entrySet().stream().max(Map.Entry.comparingByKey()).get().getValue();
+
+		return maxRepeatingSizes;
 
 	}
 
--- src/test/java/com/nick/synalogik/service/test/FileReadingTest.java
+++ src/test/java/com/nick/synalogik/service/test/FileReadingTest.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,33 +16,30 @@ import org.junit.Test;
 
 import com.nick.synalogik.service.FileReading;
 
-
 public class FileReadingTest {
 	private String fileInput = FileReading.fileInput;
 	public String splitDeliminator = "[,*;:.@?!_'\\s]+";
-	//private FileReading fileReading;
-	
-	
+
 	@Test
 	public void readFile() throws FileNotFoundException {
 		Scanner in = new Scanner(new File(fileInput));
 		FileReading fileReading = new FileReading();
-		
+
 		Assert.assertEquals(in.toString(), fileReading.readFile().toString());
 	}
-	
+
 	@Test
 	public void wordCount() throws FileNotFoundException {
 		Scanner in = new Scanner(new File(fileInput));
 		FileReading fileReading = new FileReading();
-		
+
 		List<String> listwords = new ArrayList<>();
 		while (in.hasNext()) {
 			listwords.addAll(Arrays.asList(in.next().split(splitDeliminator)));
 		}
 		Assert.assertEquals(listwords.size(), fileReading.wordCount());
 	}
-	
+
 //	@SuppressWarnings("deprecation")
 	@Test
 	public void averageWordCount() throws FileNotFoundException {
@@ -47,11 +47,11 @@ public class FileReadingTest {
 		FileReading fileReading = new FileReading();
 		int letter_count = 0;
 		List<String> listwords = new ArrayList<>();
-		
+
 		while (in.hasNext()) {
 			listwords.addAll(Arrays.asList(in.next().split(splitDeliminator)));
 		}
-		
+
 		for (String word : listwords) {
 			letter_count += word.length();
 		}
@@ -60,8 +60,114 @@ public class FileReadingTest {
 		average = Math.floor(average * 1000) / 1000;
 		Assert.assertEquals(average, fileReading.averageWordLength(), 0.001);
 	}
+
+	@Test
+	public void numWordsOfLength() throws FileNotFoundException {
+		Scanner in = new Scanner(new File(fileInput));
+		FileReading fileReading = new FileReading();
+		List<String> listwords = new ArrayList<>();
+		int[] arr = new int[7];
+
+		while (in.hasNext()) {
+			listwords.addAll(Arrays.asList(in.next().split(splitDeliminator)));
+		}
+
+		if (in != null) {
+			int numWordsOfLength_1 = 0;
+			int numWordsOfLength_2 = 0;
+			int numWordsOfLength_3 = 0;
+			int numWordsOfLength_4 = 0;
+			int numWordsOfLength_5 = 0;
+			int numWordsOfLength_7 = 0;
+			int numWordsOfLength_10 = 0;
+
+			List<String> wordsSorted = new ArrayList<String>();
+
+			for (String word : listwords) {
+				if (word.length() == 1) {
+					numWordsOfLength_1++;
+					wordsSorted.add(word);
+
+				}
+				if (word.length() == 2) {
+					numWordsOfLength_2++;
+					wordsSorted.add(word);
+
+				}
+				if (word.length() == 3) {
+					numWordsOfLength_3++;
+					wordsSorted.add(word);
+
+				}
+				if (word.length() == 4) {
+					numWordsOfLength_4++;
+					wordsSorted.add(word);
+
+				}
+
+				if (word.length() == 5) {
+					numWordsOfLength_5++;
+					wordsSorted.add(word);
+
+				}
+				if (word.length() == 7) {
+					numWordsOfLength_7++;
+					wordsSorted.add(word);
+
+				}
+				if (word.length() == 10) {
+					numWordsOfLength_10++;
+					wordsSorted.add(word);
+
+				}
+			}
+
+			arr[0] = numWordsOfLength_1;
+			arr[1] = numWordsOfLength_2;
+			arr[2] = numWordsOfLength_3;
+			arr[3] = numWordsOfLength_4;
+			arr[4] = numWordsOfLength_5;
+			arr[5] = numWordsOfLength_7;
+			arr[6] = numWordsOfLength_10;
+
+			Assert.assertEquals(arr[0], fileReading.numWordsOfLength()[0]);
+			Assert.assertEquals(arr[1], fileReading.numWordsOfLength()[1]);
+			Assert.assertEquals(arr[2], fileReading.numWordsOfLength()[2]);
+			Assert.assertEquals(arr[3], fileReading.numWordsOfLength()[3]);
+			Assert.assertEquals(arr[4], fileReading.numWordsOfLength()[4]);
+			Assert.assertEquals(arr[5], fileReading.numWordsOfLength()[5]);
+			Assert.assertEquals(arr[6], fileReading.numWordsOfLength()[6]);
+		}
+	}
 	
+	@Test
+	public void maxRepeatedSizes() throws FileNotFoundException {
+		Scanner in = new Scanner(new File(fileInput));
+		FileReading fileReading = new FileReading();
+		List<String> listwords = new ArrayList<>();
+		
+		while (in.hasNext()) {
+			listwords.addAll(Arrays.asList(in.next().split(splitDeliminator)));
+		}
+		
+		List<String> wordsSorted = new ArrayList<String>();
 
+		for (String word : listwords) {
+			if (word.length() == 1 || word.length() == 2 || word.length() == 3 || word.length() == 4 || word.length() == 5 || word.length() == 7 || word.length() == 10) {
+				wordsSorted.add(word);
+			}
+		}
 
+		String[] test = wordsSorted.toArray(new String[0]);
+
+		List<Integer> maxRepeatingSizes = Arrays.stream(test)
+				.collect(Collectors.groupingBy(String::length, Collectors.counting())).entrySet().stream()
+				.collect(Collectors.groupingBy(Map.Entry::getValue,
+						Collectors.mapping(Map.Entry::getKey, Collectors.toList())))
+				.entrySet().stream().max(Map.Entry.comparingByKey()).get().getValue();
+		
+		Assert.assertEquals(maxRepeatingSizes, fileReading.maxRepeatedSizes());
+		
+	}
 
 }
