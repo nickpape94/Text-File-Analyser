d605cb1232f45d540ff2701ee671104d09319895
nickpape94 <nick-_p@hotmail.com>
2020-01-12 11:57:37 +0000

    all working, now just need to test

--- src/main/java/com/nick/synalogik/service/FileReading.java
+++ src/main/java/com/nick/synalogik/service/FileReading.java
@@ -1,17 +1,10 @@
 package com.nick.synalogik.service;
-
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -20,7 +13,7 @@ import java.util.stream.Collectors;
 public class FileReading {
 
 	String fileInput = "C:\\Users\\Nick-\\Java-Projects\\test5.txt";
-	// String splitDeliminator = "[,*.@?!_'\\\\s]+";
+	String splitDeliminator = "[,*;:.@?!_'\\s]+";
 
 	public Scanner readFile() throws FileNotFoundException {
 		Scanner in = new Scanner(new File(fileInput));
@@ -29,30 +22,17 @@ public class FileReading {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		// new FileReading().test();
-		// new FileReading().test();
 		new FileReading().wordCount_andAverageWordLength();
 		new FileReading().numWordsOfLength();
 
 	}
 
-//	public void test() throws FileNotFoundException {
-//		Scanner in = readFile();
-//		List<String> listwords = new ArrayList<>();
-//		while (in.hasNext()) {
-//			listwords.addAll(Arrays.asList(in.next().split("\\r?\\n")));
-//
-//		}
-//
-//		// System.out.println(listwords);
-//	}
-
 	public void wordCount_andAverageWordLength() throws FileNotFoundException {
 		Scanner in = readFile();
-		int letter_count = 0;
 		List<String> listwords = new ArrayList<>();
+		int letter_count = 0;
 		while (in.hasNext()) {
-			listwords.addAll(Arrays.asList(in.next().split("\\r?\\n")));
+			listwords.addAll(Arrays.asList(in.next().split(splitDeliminator)));
 		}
 
 		// word_count = word_count + listwords.size();
@@ -65,16 +45,14 @@ public class FileReading {
 
 		System.out.println("Word count: " + listwords.size());
 		System.out.println("Average word length: " + average);
-		System.out.println(listwords);
 	}
 
 	public void numWordsOfLength() throws FileNotFoundException {
 		Scanner in = readFile();
 		List<String> listwords = new ArrayList<>();
-		String[] wordsInFile = in.next().split("\\s+");
 
 		while (in.hasNext()) {
-			listwords.addAll(Arrays.asList(in.next().split("\\r?\\n")));
+			listwords.addAll(Arrays.asList(in.next().split(splitDeliminator)));
 		}
 		if (in != null) {
 			int numWordsOfLength_1 = 0;
@@ -86,8 +64,6 @@ public class FileReading {
 			int numWordsOfLength_10 = 0;
 
 			List<String> wordsSorted = new ArrayList<String>();
-
-			// String[] wordsInFile = line.split("\\s+");
 
 			for (String word : listwords) {
 				if (word.length() == 1) {
@@ -141,6 +117,7 @@ public class FileReading {
 							Collectors.mapping(Map.Entry::getKey, Collectors.toList())))
 					.entrySet().stream().max(Map.Entry.comparingByKey()).get().getValue();
 
+			System.out.println(listwords);
 			System.out.println("Number of words of length 1: " + numWordsOfLength_1);
 			System.out.println("Number of words of length 2: " + numWordsOfLength_2);
 			System.out.println("Number of words of length 3: " + numWordsOfLength_3);
